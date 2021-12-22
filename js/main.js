e66b30ba26a1d512baf12f4266d66581e89b85f7
Paul Son <contactpaulson@gmail.com>
2021-12-21 20:28:02 -0800

    Put the form's input values into a new object. Add the nextEntryId to the object. Increment the nextEntryId on the data model. Prepend the new object to the entries in the data model. Reset the image preview's 'src' attribute. Reset the form inputs. #1

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@
 var $photoUrlInsert = document.getElementById('photoUrl');
 var $image = document.querySelector('img');
 var $formEntire = document.querySelector('.form-entire');
+var $titleInsert = document.getElementById('title');
+var $notesInsert = document.getElementById('notes');
 
 function insertNewPhoto(event) {
   var insertUrl = $photoUrlInsert.value;
@@ -12,6 +14,17 @@ function insertNewPhoto(event) {
 $photoUrlInsert.addEventListener('input', insertNewPhoto);
 
 function submitForm(event) {
+  event.preventDefault();
+  var formValue = {
+    title: $titleInsert.value,
+    image: $photoUrlInsert.value,
+    notes: $notesInsert.value,
+    id: data.nextEntryId
+  };
+  data.nextEntryId++;
+  data.entries.unshift(formValue);
+  $image.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $formEntire.reset();
 }
 
 $formEntire.addEventListener('submit', submitForm);
