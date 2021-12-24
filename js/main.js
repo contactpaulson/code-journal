b14596d65e4748c2d0e73dd2b8bcf5f56d1e358a
Paul Son <contactpaulson@gmail.com>
2021-12-23 22:25:31 -0800

    Use a loop to create a DOM tree for each journal entry in the data model and append it to the page when the 'DOMContentLoaded' event is fired. #2

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ function submitForm(event) {
 }
 
 $formEntire.addEventListener('submit', submitForm);
-
+// does setAttribute() add to or completely replace the other class values? So does it set a new value which will get rid of all styling done to that class?
 function insertEntry(entry) {
   var $newEntry = document.createElement('li');
   $newEntry.setAttribute('class', 'row');
@@ -38,8 +38,9 @@ function insertEntry(entry) {
   $newEntry.appendChild($newColumnEntry);
 
   var $newImageEntry = document.createElement('img');
-  // or is the new value going for the src attribute going to be 'data.entries.image'
-  $newImageEntry.setAttribute('src', 'entry.image');
+  // or is the new value going for the src attribute going to be 'data.entries.image'?
+  $newImageEntry.setAttribute('src', entry.image);
+  $newImageEntry.setAttribute('class', 'picture border-style');
   $newColumnEntry.appendChild($newImageEntry);
 
   var $columnEntryText = document.createElement('div');
@@ -48,13 +49,28 @@ function insertEntry(entry) {
 
   var $titleForEntries = document.createElement('h3');
   var $titleText = document.createTextNode(entry.title);
+  $titleForEntries.setAttribute('class', 'title-entries');
   $titleForEntries.appendChild($titleText);
   $columnEntryText.appendChild($titleForEntries);
 
   var $notesForEntries = document.createElement('p');
   var $notesText = document.createTextNode(entry.notes);
+  $notesForEntries.setAttribute('class', 'notes-entries');
   $notesForEntries.appendChild($notesText);
-  $columnEntryText.appendChild($notesText);
+  $columnEntryText.appendChild($notesForEntries);
 
   return $newEntry;
 }
+
+var $entriesPosition = document.querySelector('ul');
+// not really sure what to put in as the parameter for the callback function
+function addNewEntry(entry) {
+  for (var i = 0; i < data.entries.length; i++) {
+    var insertingNewEntry = insertEntry(data.entries[i]);
+    $entriesPosition.appendChild(insertingNewEntry);
+  }
+}
+// what is the event target here for this addeventlistener?
+window.addEventListener('DOMContentLoaded', addNewEntry);
+
+// how would we even check for this during the entire process?
