fb3eff4d261ebe35d132b2482b6c2a65995bef04
Paul Son <contactpaulson@gmail.com>
2021-12-23 17:14:28 -0800

    Define a function that takes a single journal entry object and returns a DOM tree that matches one of the example entries in the HTML. #2

--- js/main.js
+++ js/main.js
@@ -28,3 +28,33 @@ function submitForm(event) {
 }
 
 $formEntire.addEventListener('submit', submitForm);
+
+function insertEntry(entry) {
+  var $newEntry = document.createElement('li');
+  $newEntry.setAttribute('class', 'row');
+
+  var $newColumnEntry = document.createElement('div');
+  $newColumnEntry.setAttribute('class', 'column-half');
+  $newEntry.appendChild($newColumnEntry);
+
+  var $newImageEntry = document.createElement('img');
+  // or is the new value going for the src attribute going to be 'data.entries.image'
+  $newImageEntry.setAttribute('src', 'entry.image');
+  $newColumnEntry.appendChild($newImageEntry);
+
+  var $columnEntryText = document.createElement('div');
+  $columnEntryText.setAttribute('class', 'column-half');
+  $newEntry.appendChild($columnEntryText);
+
+  var $titleForEntries = document.createElement('h3');
+  var $titleText = document.createTextNode(entry.title);
+  $titleForEntries.appendChild($titleText);
+  $columnEntryText.appendChild($titleForEntries);
+
+  var $notesForEntries = document.createElement('p');
+  var $notesText = document.createTextNode(entry.notes);
+  $notesForEntries.appendChild($notesText);
+  $columnEntryText.appendChild($notesText);
+
+  return $newEntry;
+}
